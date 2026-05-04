cd240889f645e72d1efaac50036f43544be926ad
AshutoshDas62 <ashutoshkrdas2@gmail.com>
2026-05-05 01:34:47 +0530

    remove dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-     <Navbar />
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
